65af7ac56a8138720f5c5166238e618d4a0b9a52
pat19218 <68085020+pat19218@users.noreply.github.com>
2021-02-19 21:25:03 -0600

    cuerpo

---
+++ Labs.X/Lab4/Lab4.s
@@ -0,0 +1,201 @@
+;Archivo:	Lab4.S
+;Dispositivo:	PIC16f887
+;Autor:		Cristhofer Patzan
+;Compilador:	pic-as (v2.30), MPLABX V5.45
+;
+;Programa:	contador 4 bits por interrupciones y incr/decre del puerto
+;Hardware:	LEDs en el puerto A y displays puerto C y D Boton en el puerto B
+;
+;Creado: 19 feb, 2021
+;Última modificación: 19 feb, 2021
+
+;//////////////////////////////////////////////////////////////////////////////
+; Configuration word 1
+; PIC16F887 Configuration Bit Settings
+; Assembly source line config statements
+;//////////////////////////////////////////////////////////////////////////////
+PROCESSOR 16F887
+#include <xc.inc>
+
+; CONFIG1
+  CONFIG  FOSC = INTRC_NOCLKOUT ; Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+  CONFIG  WDTE = OFF            ; Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+  CONFIG  PWRTE = OFF           ; Power-up Timer Enable bit (PWRT disabled)
+  CONFIG  MCLRE = OFF           ; RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
+  CONFIG  CP = OFF              ; Code Protection bit (Program memory code protection is disabled)
+  CONFIG  CPD = OFF             ; Data Code Protection bit (Data memory code protection is disabled)
+  CONFIG  BOREN = OFF           ; Brown Out Reset Selection bits (BOR disabled)
+  CONFIG  IESO = OFF            ; Internal External Switchover bit (Internal/External Switchover mode is disabled)
+  CONFIG  FCMEN = OFF           ; Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+  CONFIG  LVP = ON              ; Low Voltage Programming Enable bit (RB3/PGM pin has PGM function, low voltage programming enabled)
+
+; CONFIG2
+  CONFIG  BOR4V = BOR40V        ; Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+  CONFIG  WRT = OFF             ; Flash Program Memory Self Write Enable bits (Write protection off)
+ 
+ ;------------------------------------------------------------------------------
+ ;  Macro
+ ;------------------------------------------------------------------------------
+  reiniciar_tmr0 macro
+    banksel PORTA
+    movlw   60
+    movwf   TMR0    ;ciclo de 50ms
+    bcf	    T0IF
+    endm
+   
+ ;------------------------------------------------------------------------------
+ ;  Variables
+ ;------------------------------------------------------------------------------
+ 
+ PSECT udata_bank0  ;common memory
+    cont:   DS 1 ;1 byte   
+ PSECT udata_shr  ;common memory
+    W_temp:	   DS 1 ;1 byte
+    STATUS_temp:   DS 1 ;1 byte
+
+ ;------------------------------------------------------------------------------
+ ;  Vector reset
+ ;------------------------------------------------------------------------------
+ PSECT resVect, class=CODE, abs, delta=2
+ 
+ ORG 00h    ;  posición 0000h para el reset
+ resetVec:
+    PAGESEL main
+    goto main
+
+ ;------------------------------------------------------------------------------
+ ;  Vector Interrupt
+ ;------------------------------------------------------------------------------
+ PSECT intVect, class=CODE, abs, delta=2
+ 
+ ORG 04h    ;  posición 0004h para las interrupciones
+ 
+ push:
+    movwf   W_temp
+    swapf   STATUS, W
+    movwf   STATUS_temp
+    
+ isr:
+    btfsc   T0IF
+    call    T0_int
+    
+ pop:
+    swapf   STATUS_temp, W
+    movwf   STATUS
+    swapf   W_temp, F
+    swapf   W_temp, W
+    retfie
+ ;------------------------------------------------------------------------------
+ ;	sub rutinas de interrupcion
+ ;------------------------------------------------------------------------------
+ T0_int:
+    reiniciar_tmr0  ;50ms
+    incf    cont
+    movwf   cont, W
+    sublw   20	    ;50ms * 10 = 500ms
+    btfss    ZERO
+    goto    $+2
+    clrf    cont
+    incf    PORTA
+    
+    return
+ 
+ PSECT code, delta=2, abs
+ ORG 100h   ;posicion para el código
+tabla: 
+    clrf    PCLATH
+    bsf	    PCLATH, 0	;PCLATH = 01
+    andwf   0x0f	;me aseguro q solo pasen 4 bits
+    addwf   PCL		;PC = PCL + PCLATH + w
+    retlw   11111100B	;0  posicion 0
+    retlw   01100000B	;1  posicion 1
+    retlw   11011010B	;2  posicion 2
+    retlw   11110010B	;3  posicion 3
+    retlw   01100110B	;4  posicion 4
+    retlw   10110110B	;5  posicion 5
+    retlw   10111110B	;6  posicion 6
+    retlw   11100000B	;7  posicion 7
+    retlw   11111110B	;8  posicion 8
+    retlw   11110110B	;9  posicion 9
+    retlw   11101110B	;A  posicion 10
+    retlw   00111110B	;B  posicion 11
+    retlw   10011100B	;C  posicion 12
+    retlw   01111010B	;D  posicion 13
+    retlw   10011110B	;E  posicion 14
+    retlw   10001110B	;F  posicion 15
+ 
+ ;------------------------------------------------------------------------------
+ ;  Configuracion microprcesador
+ ;------------------------------------------------------------------------------
+ 
+ main:
+    banksel ANSEL   ;Selecciono el banco donde esta ANSEL
+    clrf    ANSEL
+    clrf    ANSELH
+    
+    banksel TRISB   ;Puerto B pin 0 y 1 entradas
+    clrf    TRISB
+    bsf	    TRISB, 0
+    bsf	    TRISB, 1
+    
+    clrf    TRISC   ;Pines de salida
+    clrf    TRISD   ;Pin de salida puerto D
+    
+    clrf    TRISA   ;Pin de salida puerto A
+    bsf	    TRISA, 4
+    bsf	    TRISA, 5
+    bsf	    TRISA, 6
+    bsf	    TRISA, 7
+    
+    banksel PORTA   ;Me asegure que empiece en cero
+    clrf    PORTA
+    clrf    PORTB
+    movlw   11111100B
+    movwf   PORTC
+    movwf   PORTD
+    
+    banksel OSCCON
+    bsf	    IRCF2   ;4MHZ = 110
+    bsf	    IRCF1
+    bcf	    IRCF0
+    bsf	    SCS	    ;reloj interno activo
+    
+    call    conf_tmr0 
+    call    conf_interrupt_ena
+    banksel PORTA
+    movlw   0x00
+    movwf   cont
+    
+    
+ ;------------------------------------------------------------------------------
+ ;  loop principal
+ ;------------------------------------------------------------------------------
+ 
+ loop:
+  nop
+   
+  goto    loop
+    
+ ;------------------------------------------------------------------------------
+ ;	sub rutinas
+ ;------------------------------------------------------------------------------
+ 
+ conf_tmr0:
+    banksel TRISA
+    bcf	    T0CS    ;usar el reloj interno, temporizador
+    bcf	    PSA	    ;usar prescaler
+    bsf	    PS2
+    bsf	    PS1
+    bsf	    PS0	    ;PS = 111 /1:256
+    banksel PORTA
+    reiniciar_tmr0
+    return
+ 
+ 
+ conf_interrupt_ena:
+    bsf	    GIE
+    bsf	    T0IE
+    bcf	    T0IF
+    return
+ 
+ END
